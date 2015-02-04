13e0484a5e68ea130fa115e1c25b0c273dba53f5
Ambroise Maupate <ambroise.maupate@gmail.com>
2015-02-04 13:55:41 +0100

    Translated script to English

--- create_roadiz.default.sh
+++ create_roadiz.default.sh
@@ -2,7 +2,7 @@
 # Ambroise Maupate
 
 echo "------------------ ROADIZ CMS -------------------\n"
-echo "--- Création d'un nouveau site sur `hostname` ---\n"
+echo "--------- New Roadiz website on `hostname` ------\n"
 echo "-------------------------------------------------\n"
 
 APACHE_ROOT="/var/www/"
@@ -13,6 +13,10 @@ ROADIZ_BRANCH="master"
 THEME_URL="https://github.com/roadiz/BaseTheme.git"
 THEME_BRANCH="master"
 
+#
+# MySQL credentials
+# Change these with your own
+#
 MYSQL_HOST="localhost"
 MYSQL_USER="root"
 MYSQL_PASS="****password****"
@@ -26,13 +30,13 @@ GRUNT=`which grunt`
 
 cd $APACHE_ROOT;
 
-echo "1. Entrez le nom du nouveau site et appuyez sur [ENTRER]\nCe nom sera utilisé pour le dossier et l'utilisateur MySQL :";
+echo "1. Type your new website name and type [ENTER].\nThis name will be used for your web folder and your MySQL user and database name:";
 read destination;
 
-echo "2. Entrez le mot de passe de la base de donnée et appuyez sur [ENTRER]:";
+echo "2. Choose a password for your MySQL user and type [ENTER]:";
 read password;
 
-echo "3. Entrez le préfix de votre futur thème en CamelCase et appuyez sur [ENTRER].\nPar exemple 'MySuper' donnera un thème 'MySuperTheme':";
+echo "3. Choose a prefix for your Roadiz theme. Type it in CamelCase syntax and hit [ENTER].\nFor example 'MySuper' will generate a theme called 'MySuperTheme':";
 read theme_prefix;
 
 mkdir -p $destination;
@@ -42,48 +46,48 @@ cd $APACHE_ROOT$destination;
 # Roadiz
 #
 $GIT clone -b $ROADIZ_BRANCH $ROADIZ_URL ./;
-echo "* Téléchargement des sources - OK";
+echo "* Download latest Roadiz sources - OK";
 
 $COMPOSER install;
 $COMPOSER dumpautoload -o;
-echo "* Téléchargement des dépendances - OK";
+echo "* Download latest Roadiz dependencies - OK";
 
 cp conf/config.default.json conf/config.json;
-echo "* Copie de la configuration par défaut - OK";
+echo "* Make a copy of default configuration file - OK";
 
 bin/roadiz config --generateHtaccess
-echo "* Generation des .htaccess - OK";
+echo "* Generate .htaccess files for Apache - OK";
 
 #
 # BaseTheme
 #
 $GIT clone -b $THEME_BRANCH $THEME_URL ./themes/${theme_prefix}Theme;
-echo "* Téléchargement des sources du theme - OK";
+echo "* Download Base theme sources into themes folder - OK";
 
 cd ${APACHE_ROOT}${destination}/themes/${theme_prefix}Theme;
 
 rm -rf ${APACHE_ROOT}${destination}/themes/${theme_prefix}Theme/.git
-echo "* Suppression de l’historique Git du thème - OK";
+echo "* Delete existing Git history - OK";
 
 mv BaseThemeApp.php ${theme_prefix}ThemeApp.php
 mv static/js/BaseTheme.js static/js/${theme_prefix}Theme.js
 mv static/js/BaseTheme.min.js static/js/${theme_prefix}Theme.min.js
-echo "* Renommage des fichiers de votre thème - OK";
+echo "* Rename theme files against you theme name - OK";
 
 LC_ALL=C $FIND ./ -type f -exec $SED -i.bak -e "s/BaseTheme/${theme_prefix}Theme/g" {} \;
 LC_ALL=C $FIND ./ -type f -exec $SED -i.bak -e "s/Base theme/${theme_prefix} theme/g" {} \;
 LC_ALL=C $FIND ./ -type f -name '*.bak' -exec rm -f {} \;
-echo "* Renommage des occurences de votre thème - OK";
+echo "* Rename every occurrences of BaseTheme in your theme - OK";
 
 #
 # Grunt
 #
 cd ${APACHE_ROOT}${destination}/themes/${theme_prefix}Theme/static;
 $NPM install;
-echo "* Installation du Grunt pour votre theme - OK";
+echo "* Install Grunt for your theme - OK";
 
 $GRUNT
-echo "* Lancement du premier Grunt - OK";
+echo "* Launch Grunt for the first time - OK";
 
 
 #
@@ -93,7 +97,7 @@ cd ${APACHE_ROOT}${destination}/themes/${theme_prefix}Theme;
 $GIT init;
 $GIT add --all;
 $GIT commit -a -m "First commit";
-echo "* Réinitialisation du dépôt Git et premier commit - OK";
+echo "* Reinit Git repository and first commit - OK";
 
 #
 # Installation MySQL
@@ -108,9 +112,9 @@ SQL="${Q1}${Q2}${Q4}";
 
 
 $MYSQL -u$MYSQL_USER -p$MYSQL_PASS -e "$SQL";
-echo "* Création de la base de donnée sur `hostname` - OK\n";
+echo "* MySQL database creation on `hostname` - OK\n";
 
-	echo "--------------------------------------------------------------------------------\n"
-	echo "---- Le nouveau site '$destination' a été créé ainsi que sa base de donnée -----\n"
-	echo "---- MySQL Base: '$destination' User: '$destination' Password: '$password' -----\n"
-	echo "--------------------------------------------------------------------------------\n"
+echo "--------------------------------------------------------------------------------\n"
+echo "--- Your new website '$destination' has been created with its own database -----\n"
+echo "-- MySQL Database: '$destination' User: '$destination' Password: '$password' ---\n"
+echo "--------------------------------------------------------------------------------\n"
