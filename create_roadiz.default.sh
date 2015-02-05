a68fcb7789e9dcb3c9b89d77d17e9d610a904a6b
Maxime Constantinian <azokal@users.noreply.github.com>
2015-02-05 16:31:20 +0100

    Update create_roadiz.default.sh
    Add Color
    Add generic shebang
    Add real password

--- create_roadiz.default.sh
+++ create_roadiz.default.sh
@@ -1,9 +1,10 @@
-#!bin/bash
-# Ambroise Maupate
+#!/usr/bin/env bash
+# Autor: Ambroise Maupate
+# Contributor: Maxime Constantinian
 
-echo "------------------ ROADIZ CMS -------------------\n"
-echo "--------- New Roadiz website on `hostname` ------\n"
-echo "-------------------------------------------------\n"
+echo -e "\033[36m------------------ ROADIZ CMS -------------------\033[0m"
+echo -e "\033[36m--------- New Roadiz website on `hostname` ------\033[0m"
+echo -e "\033[36m-------------------------------------------------\033[0m"
 
 APACHE_ROOT="/var/www/"
 
@@ -30,13 +31,17 @@ GRUNT=`which grunt`
 
 cd $APACHE_ROOT;
 
-echo "1. Type your new website name and type [ENTER].\nThis name will be used for your web folder and your MySQL user and database name:";
+echo -e "\033[33m1. Type your new website name and type [ENTER].\033[0m"
+echo -e "\033[33mThis name will be used for your web folder and your MySQL user and database name:\033[0m";
 read destination;
 
-echo "2. Choose a password for your MySQL user and type [ENTER]:";
+echo -e "\033[33m2. Choose a password for your MySQL user and type [ENTER]:\033[0m";
+stty -echo
 read password;
+stty echo
 
-echo "3. Choose a prefix for your Roadiz theme. Type it in CamelCase syntax and hit [ENTER].\nFor example 'MySuper' will generate a theme called 'MySuperTheme':";
+echo -e "\033[33m3. Choose a prefix for your Roadiz theme. Type it in CamelCase syntax and hit [ENTER].\033[0m"
+echo -e "\033[33mFor example 'MySuper' will generate a theme called 'MySuperTheme':\033[0m";
 read theme_prefix;
 
 mkdir -p $destination;
@@ -46,48 +51,48 @@ cd $APACHE_ROOT$destination;
 # Roadiz
 #
 $GIT clone -b $ROADIZ_BRANCH $ROADIZ_URL ./;
-echo "* Download latest Roadiz sources - OK";
+echo -e "\033[32m* Download latest Roadiz sources - OK\033[0m";
 
 $COMPOSER install;
 $COMPOSER dumpautoload -o;
-echo "* Download latest Roadiz dependencies - OK";
+echo -e "\033[32m* Download latest Roadiz dependencies - OK\033[0m";
 
 cp conf/config.default.json conf/config.json;
-echo "* Make a copy of default configuration file - OK";
+echo -e "\033[32m* Make a copy of default configuration file - OK\033[0m";
 
 bin/roadiz config --generateHtaccess
-echo "* Generate .htaccess files for Apache - OK";
+echo -e "\033[32m* Generate .htaccess files for Apache - OK\033[0m";
 
 #
 # BaseTheme
 #
 $GIT clone -b $THEME_BRANCH $THEME_URL ./themes/${theme_prefix}Theme;
-echo "* Download Base theme sources into themes folder - OK";
+echo -e "\033[32m* Download Base theme sources into themes folder - OK\033[0m";
 
 cd ${APACHE_ROOT}${destination}/themes/${theme_prefix}Theme;
 
 rm -rf ${APACHE_ROOT}${destination}/themes/${theme_prefix}Theme/.git
-echo "* Delete existing Git history - OK";
+echo -e "\033[32m* Delete existing Git history - OK\033[0m";
 
 mv BaseThemeApp.php ${theme_prefix}ThemeApp.php
 mv static/js/BaseTheme.js static/js/${theme_prefix}Theme.js
 mv static/js/BaseTheme.min.js static/js/${theme_prefix}Theme.min.js
-echo "* Rename theme files against you theme name - OK";
+echo -e "\033[32m* Rename theme files against you theme name - OK\033[0m";
 
 LC_ALL=C $FIND ./ -type f -exec $SED -i.bak -e "s/BaseTheme/${theme_prefix}Theme/g" {} \;
 LC_ALL=C $FIND ./ -type f -exec $SED -i.bak -e "s/Base theme/${theme_prefix} theme/g" {} \;
 LC_ALL=C $FIND ./ -type f -name '*.bak' -exec rm -f {} \;
-echo "* Rename every occurrences of BaseTheme in your theme - OK";
+echo -e "\033[32m* Rename every occurrences of BaseTheme in your theme - OK\033[0m";
 
 #
 # Grunt
 #
 cd ${APACHE_ROOT}${destination}/themes/${theme_prefix}Theme/static;
 $NPM install;
-echo "* Install Grunt for your theme - OK";
+echo -e "\033[32m* Install Grunt for your theme - OK\033[0m";
 
 $GRUNT
-echo "* Launch Grunt for the first time - OK";
+echo -e "\033[32m* Launch Grunt for the first time - OK\033[0m";
 
 
 #
@@ -97,7 +102,7 @@ cd ${APACHE_ROOT}${destination}/themes/${theme_prefix}Theme;
 $GIT init;
 $GIT add --all;
 $GIT commit -a -m "First commit";
-echo "* Reinit Git repository and first commit - OK";
+echo -e "\033[32m* Reinit Git repository and first commit - OK\033[0m";
 
 #
 # Installation MySQL
@@ -112,9 +117,9 @@ SQL="${Q1}${Q2}${Q4}";
 
 
 $MYSQL -u$MYSQL_USER -p$MYSQL_PASS -e "$SQL";
-echo "* MySQL database creation on `hostname` - OK\n";
+echo -e "\033[32m* MySQL database creation on `hostname` - OK\033[0m";
 
-echo "--------------------------------------------------------------------------------\n"
-echo "--- Your new website '$destination' has been created with its own database -----\n"
-echo "-- MySQL Database: '$destination' User: '$destination' Password: '$password' ---\n"
-echo "--------------------------------------------------------------------------------\n"
+echo -e "\033[33m--------------------------------------------------------------------------------\033[0m"
+echo -e "\033[33m--- Your new website '$destination' has been created with its own database -----\033[0m"
+echo -e "\033[33m-- MySQL Database: '$destination' User: '$destination' Password: '$password' ---\033[0m"
+echo -e "\033[33m--------------------------------------------------------------------------------\033[0m"
